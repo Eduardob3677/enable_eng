e91863d0fd8ffa59503215405dd4075a9e4944a3
Rafael Alvarado Navarro <ealvaradobucio26@gmail.com>
2025-11-20 22:10:38 -0600

    Enable ADB and USB debugging properties

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,3 +2,7 @@ resetprop ro.secure 0
 resetprop ro.adb.secure 0
 resetprop ro.debuggable 1
 resetprop ro.kernel.android.checkjni 1
+resetprop persist.service.adb.enable 1
+resetprop persist.service.debuggable 0
+resetprop persist.sys.usb.config mtp,adb
+resetprop service.adb.tcp.port 33445
